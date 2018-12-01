514b420da93212d389f630f3a3bbdd9968309cb0
VIBHOR94 <vibhorgupta2008@aol.in>
2018-12-02 02:22:20 +0530

    Add example for beforeEach, beforeAll, afterEach and afterAll

--- src/components/QuestionList.spec.js
+++ src/components/QuestionList.spec.js
@@ -1,5 +1,20 @@
 describe('The question list', () => {
+	beforeEach(() => {
+		console.log("Before Each")
+	})
+	beforeAll(() => {
+		console.log("Before All")
+	})
+	afterEach(() => {
+		console.log("After Each")
+	})
+	afterAll(() => {
+		console.log("After all")
+	})
 	it('should display a list of items', () => {
 		expect(2+2).toEqual(4)
+	})
+	it('should br the meaning of the life', () => {
+		expect(2+4).toEqual(6)
 	})
 })
